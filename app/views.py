1c46f35797b5518e07b56343f5b800cf4dea8206
Ravi <rrpatel1311@gmail.com>
2022-03-20 19:00:50 +0530

    Initital Commit

---
+++ app/views.py
@@ -0,0 +1,350 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced, total_cost
+from .forms import CustomerRegistrationForm, CustomerProfileForm
+from django.contrib import messages
+from django.db.models import Q
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+# def home(request):
+#  return render(request, 'app/home.html')
+
+class ProductView(View):
+    def get(self, request):
+        totalitem = 0
+        Men = Product.objects.filter(category='F-M')
+        Women = Product.objects.filter(category='F-W')
+        Makeup = Product.objects.filter(category='B-M')
+        Fragrances = Product.objects.filter(category='B-F')
+        Mobile = Product.objects.filter(category='E-M')
+        Laptop = Product.objects.filter(category='E-L')
+        TV = Product.objects.filter(category='E-T')
+        Headphone = Product.objects.filter(category='E-H')
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
+        return render(request, 'app/home.html',
+                      {'Men': Men, 'Women': Women, 'Makeup': Makeup, 'Fragrances': Fragrances, 'Mobile': Mobile,
+                       'Laptop': Laptop, 'TV': TV, 'Headphone': Headphone, 'totalitem':totalitem})
+
+
+# def product_detail(request):
+#     return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        totalitem = 0
+        product = Product.objects.get(pk=pk)
+        item_already_in_cart = False
+        if request.user.is_authenticated:
+         totalitem = len(Cart.objects.filter(user=request.user))
+         item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
+
+        return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart':item_already_in_cart, 'totalitem':totalitem})
+
+
+@login_required
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+
+@login_required
+def show_cart(request):
+    totalitem = 0
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        # print(cart)
+        amount = 0.0
+        shipping_amount = 50.0
+        total_amount = 0.0
+        cart_product = [p for p in Cart.objects.all() if p.user == user]
+        # print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                tempamount = (p.quantity * p.product.discounted_price)
+                amount += tempamount
+                total_amount = amount + shipping_amount
+                totalitem = len(Cart.objects.filter(user=request.user))
+            return render(request, 'app/addtocart.html', {'carts': cart, 'totalamount': total_amount, 'amount': amount, 'totalitem': totalitem})
+        else:
+            return render(request, 'app/emptycart.html')
+
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity += 1
+        c.save()
+        amount = 0.0
+        shipping_amount = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.discounted_price)
+            amount += tempamount
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount,
+        }
+        return JsonResponse(data)
+
+
+def minus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity -= 1
+        c.save()
+        amount = 0.0
+        shipping_amount = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.discounted_price)
+            amount += tempamount
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount,
+        }
+        return JsonResponse(data)
+
+
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        amount = 0.0
+        shipping_amount = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.discounted_price)
+            amount += tempamount
+
+        data = {
+            'amount': amount,
+            'totalamount': amount + shipping_amount,
+        }
+        return JsonResponse(data)
+
+
+def buy_now(request):
+    totalitem = 0
+    return render(request, 'app/buynow.html')
+
+
+# def profile(request):
+#     return render(request, 'app/profile.html')
+
+@login_required
+def address(request):
+    totalitem = 0
+    add = Customer.objects.filter(user=request.user)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/address.html', {'add': add, 'active': 'btn-danger', 'totalitem':totalitem})
+
+
+@login_required
+def orders(request):
+    totalitem = 0
+    op = OrderPlaced.objects.filter(user=request.user)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/orders.html', {'order_placed': op, 'totalitem':totalitem})
+
+
+# def change_password(request):
+#     return render(request, 'app/changepassword.html')
+
+
+def men(request, data=None):
+    totalitem = 0
+    if data == None:
+        Men = Product.objects.filter(category='F-M')
+    elif data == 'HIGHLANDER' or data == 'RDST':
+        Men = Product.objects.filter(category='F-M').filter(brand=data)
+    elif data == 'below':
+        Men = Product.objects.filter(category='F-M').filter(discounted_price__lt=700)
+    elif data == 'above':
+        Men = Product.objects.filter(category='F-M').filter(discounted_price__gt=700)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/men.html', {'Men': Men, 'totalitem':totalitem})
+
+
+def women(request, data=None):
+    totalitem = 0
+    if data == None:
+        Women = Product.objects.filter(category='F-W')
+    elif data == 'SASSAFRAS' or data == 'Inddus':
+        Women = Product.objects.filter(category='F-W').filter(brand=data)
+    elif data == 'below':
+        Women = Product.objects.filter(category='F-W').filter(discounted_price__lt=1000)
+    elif data == 'above':
+        Women = Product.objects.filter(category='F-W').filter(discounted_price__gt=1000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/women.html', {'Women': Women, 'totalitem':totalitem})
+
+
+def makeup(request, data=None):
+    totalitem = 0
+    if data == None:
+        Makeup = Product.objects.filter(category='B-M')
+    elif data == 'dyson' or data == 'Philips':
+        Makeup = Product.objects.filter(category='B-M').filter(brand=data)
+    elif data == 'below':
+        Makeup = Product.objects.filter(category='B-M').filter(discounted_price__lt=1000)
+    elif data == 'above':
+        Makeup = Product.objects.filter(category='B-M').filter(discounted_price__gt=1000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/makeup.html', {'Makeup': Makeup, 'totalitem':totalitem})
+
+
+def fragrances(request, data=None):
+    totalitem = 0
+    if data == None:
+        Fragrances = Product.objects.filter(category='B-F')
+    elif data == 'Denver' or data == 'JAGUAR':
+        Fragrances = Product.objects.filter(category='B-F').filter(brand=data)
+    elif data == 'below':
+        Fragrances = Product.objects.filter(category='B-F').filter(discounted_price__lt=1000)
+    elif data == 'above':
+        Fragrances = Product.objects.filter(category='B-F').filter(discounted_price__gt=1000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/fragrances.html', {'Fragrances': Fragrances, 'totalitem':totalitem})
+
+
+def mobile(request, data=None):
+    totalitem = 0
+    if data == None:
+        Mobile = Product.objects.filter(category='E-M')
+    elif data == 'MI' or data == 'SAMSUNG':
+        Mobile = Product.objects.filter(category='E-M').filter(brand=data)
+    elif data == 'below':
+        Mobile = Product.objects.filter(category='E-M').filter(discounted_price__lt=10000)
+    elif data == 'above':
+        Mobile = Product.objects.filter(category='E-M').filter(discounted_price__gt=10000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/mobile.html', {'Mobile': Mobile, 'totalitem':totalitem})
+
+
+def laptop(request, data=None):
+    totalitem = 0
+    if data == None:
+        Laptop = Product.objects.filter(category='E-L')
+    elif data == 'DELL' or data == 'HP':
+        Laptop = Product.objects.filter(category='E-L').filter(brand=data)
+    elif data == 'below':
+        Laptop = Product.objects.filter(category='E-L').filter(discounted_price__lt=35000)
+    elif data == 'above':
+        Laptop = Product.objects.filter(category='E-L').filter(discounted_price__gt=35000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/laptop.html', {'Laptop': Laptop, 'totalitem':totalitem})
+
+
+def tv(request, data=None):
+    totalitem = 0
+    if data == None:
+        TV = Product.objects.filter(category='E-T')
+    elif data == 'MI' or data == 'OP':
+        TV = Product.objects.filter(category='E-T').filter(brand=data)
+    elif data == 'below':
+        TV = Product.objects.filter(category='E-T').filter(discounted_price__lt=30000)
+    elif data == 'above':
+        TV = Product.objects.filter(category='E-T').filter(discounted_price__gt=30000)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/tv.html', {'TV': TV, 'totalitem':totalitem})
+
+
+def headphone(request, data=None):
+    totalitem = 0
+    if data == None:
+        Headphone = Product.objects.filter(category='E-H')
+    elif data == 'BoAt' or data == 'BLT':
+        Headphone = Product.objects.filter(category='E-H').filter(brand=data)
+    elif data == 'below':
+        Headphone = Product.objects.filter(category='E-H').filter(discounted_price__lt=1500)
+    elif data == 'above':
+        Headphone = Product.objects.filter(category='E-H').filter(discounted_price__gt=1500)
+    totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/headphone.html', {'Headphone': Headphone, 'totalitem':totalitem})
+
+
+# def login(request):
+#     return render(request, 'app/login.html')
+
+
+# def customerregistration(request):
+#     return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulations - Registration Success')
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+
+@login_required
+def checkout(request):
+    totalitem = 0
+    user = request.user
+    add = Customer.objects.filter(user=user)
+    cart_items = Cart.objects.filter(user=user)
+    amount = 0.0
+    shipping_amount = 50.0
+    totalamount = 0.0
+    cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+    if cart_product:
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.discounted_price)
+            amount += tempamount
+        totalamount = amount + shipping_amount
+        totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/checkout.html', {'add': add, 'totalamount': totalamount, 'cart_items': cart_items, 'totalitem':totalitem})
+
+
+@login_required
+def payment_done(request):
+    user = request.user
+    custid = request.GET.get('custid')
+    customer = Customer.objects.get(id=custid)
+    cart = Cart.objects.filter(user=user)
+    for c in cart:
+        OrderPlaced(user=user, customer=customer, product=c.product, quantity=c.quantity).save()
+        c.delete()
+    return redirect("orders")
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', {'form': form, 'active': 'btn-danger'})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'Congratulations!! Profile Updated.')
+        return render(request, 'app/profile.html', {'form': form, 'active': 'btn-danger'})
